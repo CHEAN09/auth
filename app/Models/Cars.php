e129bb8f9eb3fdbeb5e2a337e5736587e111bb7c
CHEAN09 <supermanverano@gmail.com>
2021-07-14 09:35:02 +0800

    first commit

---
+++ app/Models/Cars.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cars extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+        'description',
+        'brand',
+        'acquired_on',
+        'status'
+    ];
+    public function container() {
+        return $this->belongsTo('App\Models\Cars', 'contained_in', 'id');
+    }
+}
